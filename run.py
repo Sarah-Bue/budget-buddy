3258e320a1eabaac673d2d3e7f2401a03a06c150
Sarah-Bue Bue <buettner-sarah@gmx.net>
2024-05-17 20:29:48 +0000

    feat: add main_menu function with two options

--- run.py
+++ run.py
@@ -18,3 +18,41 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('expense_tracker')
 expenses = SHEET.worksheet("expenses")
 data = expenses.get_all_values()
+
+
+def main_menu():
+    """
+    Runs the main menu of the program.
+    Allows the user to navigate to one of two sub-menus.
+    """
+    while True:
+        print()
+        print("*** Main Menu *** \n")
+        print("Please select one of the options:")
+        print("1. Add Expenses")
+        print("2. View Expenses")
+        print()
+
+        try:
+            user_choice = input("> ")
+            if user_choice == "1":
+                print()
+                add_expenses()
+                break
+                
+            elif user_choice == "2":
+                print()
+                view_expenses()
+                break
+
+            else:
+                raise ValueError(
+                    f"Please select one of the options (1-2)."
+                )
+
+        except ValueError as e:
+            print()
+            print(f"Invalid data: {e}\n")
+
+
+main_menu()
